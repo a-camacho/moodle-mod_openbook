e8ad991ea53a1f29069f97d78bd6df5207d9f0f5
Luca Bösch <luca.boesch@bfh.ch>
2025-11-20 15:06:40 +0100

    Added thirdpartylibs.xml document.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2025111204;
+$plugin->version  = 2025111205;
 $plugin->requires = 2024100700;
 $plugin->component = 'mod_openbook';
 $plugin->maturity = MATURITY_STABLE;
