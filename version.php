390578f715dce7e87cf292d95e360369b7050a28
Luca Bösch <luca.boesch@bfh.ch>
2025-11-20 14:29:30 +0100

    Ignore library files for Moodle Code checks.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2025111203;
+$plugin->version  = 2025111204;
 $plugin->requires = 2024100700;
 $plugin->component = 'mod_openbook';
 $plugin->maturity = MATURITY_STABLE;
