8f886c2a771e65dbefb61fc5d092e79491648579
André Camacho <andrecamacho@me.com>
2026-05-28 11:14:12 +0200

    Remove dead importfrom / teamsubmission / register_in_agrp code
    The plugin is a fork of mod_publication / mod_grouptool. The schema
    has no 'importfrom' column on {openbook}, yet several leftover code
    paths referenced it (triggering 'Undefined property' warnings at
    runtime), and a number of test wrappers called parent methods that do
    not exist on the openbook class (any test reaching them would fatal).
    
    * Delete the unused get_submissionmembers() method on openbook (the
      only branch that mattered called \$DB->get_field('assign', ...,
      ['id' => importfrom])).
    * In download_zip(), drop the importfrom/teamsubmission branching;
      \$teamsubmission was hard-coded to false everywhere. Always iterate
      uploaders via get_users() and per-user metadata via fullname().
    * Remove the never-set 'protected \$teamsubmission' member.
    * In restore_openbook_stepslib.php, drop the '\$data->importfrom = -1'
      assignment that wrote a column that does not exist.
    * In classes/local/tests/openbook.php, delete the testable_*
      wrappers (register_in_agrp, unregister_from_agrp,
      qualifies_for_groupchange, can_change_group, change_group,
      add_registration, add_queue_entry) that all called nonexistent
      parent methods. Keep get_openbook() and get_context(), which are
      real helpers used by the unit tests.
    * Drop a stray teamsubmission comment in filestable/base.php.

--- classes/local/tests/openbook.php
+++ classes/local/tests/openbook.php
@@ -26,16 +26,9 @@
 namespace mod_openbook\local\tests;
 
 use stdClass;
-use coding_exception;
-use dml_exception;
-use required_capability_exception;
-use Exception;
 
 /**
- * Test subclass that makes all the protected methods we want to test public.
- *
- * Please ignore the nasty code in here just catching all kind of exceptions and then throwing them again, it's just to shut up
- * code-checker about "unnecessary method overrides" which we need to make the methods under test publicly available!
+ * Test subclass that exposes selected protected members for unit tests.
  *
  * @package       mod_openbook
  * @author        University of Geneva, E-Learning Team
@@ -51,120 +44,6 @@ class openbook extends \openbook {
      */
     public function get_openbook() {
         return $this->instance;
-    }
-
-    /**
-     * Override method to be available for testing!
-     *
-     * @param int $agrpid active-group-id to register/queue user to
-     * @param int $userid user to register/queue
-     * @param bool $previewonly optional don't act, just return a preview
-     * @return string status message
-     * @throws coding_exception
-     * @throws dml_exception
-     * @throws required_capability_exception
-     */
-    public function testable_register_in_agrp($agrpid, $userid = 0, $previewonly = false) {
-        return parent::register_in_agrp($agrpid, $userid, $previewonly);
-    }
-
-    /**
-     * Override method to be available for testing!
-     *
-     * @param int $agrpid active-group-id to unregister/unqueue user from
-     * @param int $userid user to unregister/unqueue
-     * @param bool $previewonly (optional) don't act, just return a preview
-     * @return string $message if everything went right
-     * @throws coding_exception
-     * @throws dml_exception
-     * @throws required_capability_exception
-     */
-    public function testable_unregister_from_agrp($agrpid, $userid = 0, $previewonly = false) {
-        return parent::unregister_from_agrp($agrpid, $userid, $previewonly);
-    }
-
-    /**
-     * Override method to be available for testing!
-     *
-     * @param int $agrpid ID of the active group
-     * @param int $userid ID of user to queue or null (then $USER->id is used)
-     * @return bool whether or not user qualifies for a group change
-     * @throws Exception
-     */
-    public function testable_qualifies_for_groupchange($agrpid, $userid) {
-        return parent::qualifies_for_groupchange($agrpid, $userid);
-    }
-
-    /**
-     * Override method to be available for testing!
-     *
-     * @param int $agrpid ID of the active group
-     * @param int $userid (optional) ID of user to queue or null (then $USER->id is used)
-     * @param stdClass $message (optional) cached data for the language strings
-     * @param int $oldagrpid (optional) ID of former active group
-     * @return string status message
-     * @throws coding_exception
-     * @throws dml_exception
-     * @throws exceedgroupqueuelimit
-     * @throws exceeduserqueuelimit
-     * @throws exceeduserreglimit
-     * @throws registration
-     * @throws regpresent
-     * @throws required_capability_exception
-     */
-    public function testable_can_change_group($agrpid, $userid = 0, $message = null, $oldagrpid = null) {
-        return parent::can_change_group($agrpid, $userid, $message, $oldagrpid);
-    }
-
-    /**
-     * Override method to be available for testing!
-     *
-     * @param int $agrpid ID of active group to change to
-     * @param int $userid (optional) ID of user to change group for or null ($USER->id is used).
-     * @param stdClass $message (optional) prepared message object containing username and groupname or null.
-     * @param int $oldagrpid (optional) ID of former active group
-     * @return string success message
-     * @throws coding_exception
-     * @throws dml_exception
-     * @throws exceedgroupqueuelimit
-     * @throws exceeduserqueuelimit
-     * @throws exceeduserreglimit
-     * @throws registration
-     * @throws regpresent
-     * @throws required_capability_exception
-     */
-    public function testable_change_group($agrpid, $userid = null, $message = null, $oldagrpid = null) {
-        return parent::change_group($agrpid, $userid, $message, $oldagrpid);
-    }
-
-    /**
-     * Override method to be available for testing!
-     *
-     * @param int $agrpid
-     * @param int $userid
-     * @param stdClass $message
-     * @return string
-     * @throws coding_exception
-     * @throws dml_exception
-     * @throws required_capability_exception
-     */
-    public function testable_add_registration($agrpid, $userid, $message) {
-        return parent::add_registration($agrpid, $userid, $message);
-    }
-
-    /**
-     * Override method to be available for testing!
-     *
-     * @param int $agrpid
-     * @param int $userid
-     * @param stdClass $message
-     * @return string
-     * @throws coding_exception
-     * @throws dml_exception
-     * @throws required_capability_exception
-     */
-    public function testable_add_queue_entry($agrpid, $userid, $message) {
-        return parent::add_queue_entry($agrpid, $userid, $message);
     }
 
     /**
